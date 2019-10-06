4e7a4d12cbcc4fb43d2623ada4a41876cb5b92c3
JustSanya <alexander.shyrykalov@gmail.com>
2019-10-06 17:30:53 +0300

    added scroll to top button

--- Tajem/js/main.js
+++ Tajem/js/main.js
@@ -2,3 +2,24 @@ $('#burger-trigger').on('click', function (e) {
     e.preventDefault();
     $('#myTopnav').toggleClass('responsive');
 });
+
+
+$(function() {
+
+    let scrollBtn = $('#scroll-btn');
+
+    scrollBtn.on('click', function () {
+        $("html, body").animate({ scrollTop: 0 }, 400);
+        return false;
+    });
+
+    $(window).on('scroll', function(){
+        if ($(document).scrollTop() > 400){
+            scrollBtn.fadeIn();
+        } else {
+            scrollBtn.fadeOut();
+        }
+    })
+
+});
+
