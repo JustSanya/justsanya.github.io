88e2fb31259afa15b3efa3e3321cfb6118467f04
JustSanya <alexander.shyrykalov@gmail.com>
2020-01-09 04:45:58 +0200

    Updates

---
+++ precache-manifest.8c15461b51af0d99efc78c7060866b92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe4fcb090a8909dbcedd0ef68f78309",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "b2d504c7356b10d463d2",
+    "url": "/react/static/css/main.50c226aa.chunk.css"
+  },
+  {
+    "revision": "477372a12bf6535c9ba4",
+    "url": "/react/static/js/2.8cbfa97e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react/static/js/2.8cbfa97e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2d504c7356b10d463d2",
+    "url": "/react/static/js/main.fdd81888.chunk.js"
+  },
+  {
+    "revision": "ddbe36d421685672a5a6",
+    "url": "/react/static/js/runtime-main.00fb0d8c.js"
+  }
+]);
