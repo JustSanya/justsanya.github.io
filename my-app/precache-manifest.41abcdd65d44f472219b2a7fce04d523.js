945acda4407cf577408b8ea08c975a1793cb0db6
JustSanya <alexander.shyrykalov@gmail.com>
2020-01-10 03:30:07 +0200

    upd

---
+++ my-app/precache-manifest.41abcdd65d44f472219b2a7fce04d523.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510901ba372641dae8cfd27a16cdcee5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "075cd1721993517c109b",
+    "url": "/my-app/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "27930898509d6e57ee3e",
+    "url": "/my-app/static/css/main.9f9b8b7c.chunk.css"
+  },
+  {
+    "revision": "075cd1721993517c109b",
+    "url": "/my-app/static/js/2.f6a58906.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/my-app/static/js/2.f6a58906.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27930898509d6e57ee3e",
+    "url": "/my-app/static/js/main.2ede22fa.chunk.js"
+  },
+  {
+    "revision": "44048a88bd550b975a49",
+    "url": "/my-app/static/js/runtime-main.f451602f.js"
+  }
+]);
